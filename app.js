1e1733e555516ff68f49959919633d0ef0b7a61b
Siddhartha Khatri <khatri.snk@gmail.com>
2019-07-03 00:47:55 +0530

    Added pug as template engin

--- app.js
+++ app.js
@@ -10,23 +10,26 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
 const rootDir = require('./utils/path')
-const adminRouter = require('./routes/admin')
-const shopRouter = require('./routes/shop')
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'pug')
+app.set('views', 'views')
 
 app.use((req, res, next) => {
-    console.log('This custom middleware call every time')
+    // console.log('This custom middleware call every time')
     next()
 })
-app.use('/admin', adminRouter)
-app.use(shopRouter)
+app.use('/admin', adminRoutes.router)
+app.use(shopRoutes)
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
+    // res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
+    res.status(404).render('404', { pageTitle: '404' })
 })
 
 app.listen(3000)
--- routes/shop.js
+++ routes/shop.js
@@ -2,12 +2,14 @@ const express = require('express')
 const path  = require('path')
 
 const rootDir = require('../utils/path')
+const adminRoutes = require('./admin')
 
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
     // res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'))
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+    // res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+    res.render('shop', { products: adminRoutes.products, pageTitle: 'Shop', path: "/" })
 })
 
 module.exports = router
